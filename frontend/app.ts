3b5d443cfa400775eca55f9ebb59a805d286e2f8
renearias <rene_811@hotmail.com>
2016-05-03 17:34:14 -0500

    frontend folder

---
+++ frontend/app.ts
@@ -0,0 +1,19 @@
+import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {Core} from './app/core/core';
+import {ErrorPage} from './app/error/error';
+import {LoginPage} from './app/login/login';
+declare var jQuery: any;
+
+@Component({
+  selector: 'body',
+  directives: [ROUTER_DIRECTIVES],
+  template: require('./app.html')
+})
+@RouteConfig([
+  { path: '/app/...', component: Core, name: 'App', useAsDefault: true },
+  { path: '/error', component: ErrorPage, name: 'ErrorPage' },
+  { path: '/login', component: LoginPage, name: 'LoginPage' }
+])
+export class App {
+}
